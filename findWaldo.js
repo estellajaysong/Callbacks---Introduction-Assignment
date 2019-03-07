294d82994ddfeacea40799b3e6f27813e48de849
Estella Song <estellajaysong@gmail.com>
2019-03-07 19:44:06 +0000

    refactoring using forEach loop

--- findWaldo.js
+++ findWaldo.js
@@ -1,13 +1,7 @@
-function findWaldo(arr, found) {
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] === "Waldo") {
-        found(i);   // execute callback
-      }
-    }
+var array = ["Alice", "Bob", "Waldo", "Winston"]
+
+array.forEach(function (name, index) {
+  if (name === "Waldo") {
+    console.log("Found Waldo at index " + index + "!")
   }
-  
-  function actionWhenFound(i) {
-    console.log("Found Waldo at index " + i + "!");
-  }
-  
-  findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+})
